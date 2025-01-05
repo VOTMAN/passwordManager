aedb3d5df2d74090288cb63b911a365758f66969
Arnav <76613041+VOTMAN@users.noreply.github.com>
2025-01-05 21:52:22 +0530

    Establish Connection b/w backend and frontend

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,44 @@
-import bcrypt
-import py
-print(bcrypt.gensalt())
+import bcrypt 
+import psycopg2 # type: ignore
+from flask import Flask, jsonify, request # type: ignore
+from flask_cors import CORS # type: ignore
+
+app = Flask(__name__)
+CORS(app)
+
+def getDbConnection():
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="hxur4752", port=5432)
+    return conn
+
+
+def createTable():
+    conn = getDbConnection()
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                hashed_pass TEXT NOT NULL
+    ) 
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@app.route("/", methods=["POST"])
+def setUsers(username='bro', password='hello'):
+    conn = getDbConnection()
+    cur = conn.cursor()
+    salt = bcrypt.gensalt(14)
+    
+    print(username, password)
+
+    password = password.encode('ascii')
+    print(password)
+
+    hashedPassword = bcrypt.hashpw(password, salt)
+    print(hashedPassword)
+    print(bcrypt.checkpw(password, hashedPassword))
+
+    return jsonify({'msg': 'User inserted Successfully'})
+
