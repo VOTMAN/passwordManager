94f11fc3e0c201f92f98b8b3743e8922c1656b0d
Arnav <76613041+VOTMAN@users.noreply.github.com>
2025-01-16 12:36:05 +0530

    Create home.css file in styles folder

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom"
-import { useState, useEffect } from "react"
 
 const Home = () => {
-  
+
   return(
-    <>
-      <h1>The Password Manager</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ullam velit veritatis, aspernatur laudantium non aliquid ratione reprehenderit omnis possimus porro consequatur dolorem consequuntur dolore dolorum itaque facilis quaerat beatae.</p>      
-      <div>
-        <Link to="/Register">Register</Link>
-        <br/>
-        <Link to="/Login">Login</Link>
+    <div>
+      <div className="navbar">
+        <h3>Logo</h3>
+        <div className="nav-links">
+          <Link to="/Register" className="nav-item">Register</Link>
+          <Link to="/Login" className="nav-item">Login</Link>
+        </div>
       </div>
-    </>
+      <h1 className="page-title">The Password Manager</h1>
+      <p className="page-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ullam velit veritatis, aspernatur laudantium non aliquid ratione reprehenderit omnis possimus porro consequatur dolorem consequuntur dolore dolorum itaque facilis quaerat beatae.</p>      
+    </div>
   )
 }
 
