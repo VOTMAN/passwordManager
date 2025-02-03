b1e11f9ab32bfdd4c01735ec3b2d1ac1e41102c6
Arnav <76613041+VOTMAN@users.noreply.github.com>
2025-02-03 20:09:48 +0530

    See prev app commit

--- backend/server.py
+++ backend/server.py
@@ -6,9 +6,12 @@ from datetime import timedelta
 from flask import Flask, jsonify, request
 from flask_cors import CORS 
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
+
 
 CORS(app, supports_credentials=True, resources={
     r"/api/*": {
@@ -20,6 +23,17 @@ CORS(app, supports_credentials=True, resources={
 
 # CORS(app)
 
+def getDbConn():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"), 
+        dbname=os.getenv("DB_NAME"), 
+        user=os.getenv("DB_USER"), 
+        password=os.getenv("DB_PASS"), 
+        port=os.getenv("DB_PORT"),
+    )
+    return conn
+    # cur = conn.cursor()
+
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
@@ -29,114 +43,98 @@ key = str(encrypt_key).encode()
 
 jwt = JWTManager(app)
 
-def getDbConnection():
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"), 
-        dbname=os.getenv("DB_NAME"), 
-        user=os.getenv("DB_USER"), 
-        password=os.getenv("DB_PASS"), 
-        port=os.getenv("DB_PORT"),
-    )
-
-
-    return conn
 
 def createTables():
-    conn = getDbConnection()
-    cur = conn.cursor()
+    conn = getDbConn()
+    with conn.cursor() as cur:
+        cur.execute("""CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        hashed_pass TEXT NOT NULL
+    );""")
+        
+        cur.execute("""CREATE TABLE IF NOT EXISTS passwords (
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id),
+        website VARCHAR(255) NOT NULL,
+        w_user VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    hashed_pass TEXT NOT NULL
-);""")
-    
-    cur.execute("""CREATE TABLE IF NOT EXISTS passwords (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    website VARCHAR(255) NOT NULL,
-    w_user VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);""")
-
-    conn.commit()
-
-    cur.close()
-    conn.close()
+        conn.commit()
 
 @app.route("/api/reg", methods=["POST"])
 
 def registerUsers():
-    try:
-        createTables()
-        data = request.json
-        username = data.get('username')
-        password = data.get('password')
+    conn = getDbConn()
+    with conn.cursor() as cur:
+        try:
+            createTables()
+            data = request.json
+            username = data.get('username')
+            password = data.get('password')
 
-        conn = getDbConnection()
-        cur = conn.cursor()
+            # conn = getDbConnection()
+            # cur = conn.cursor()
 
-        if not username or not password:
-            return jsonify({"error": "Username and password are required"}), 400
-        
-        
-        cur.execute("SELECT username from users WHERE username = %s", (username,))
-        user_exists = cur.fetchone()
-        
-        if user_exists:
-            return jsonify({"error": "The username is already exists"}), 409
-        
-        hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14))
-        password = None
+            if not username or not password:
+                return jsonify({"error": "Username and password are required"}), 400
+            
+            
+            cur.execute("SELECT username from users WHERE username = %s", (username,))
+            user_exists = cur.fetchone()
+            
+            if user_exists:
+                return jsonify({"error": "The username is already exists"}), 409
+            
+            hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14))
+            password = None
 
-        cur.execute("INSERT INTO users (username, hashed_pass) VALUES (%s, %s) ", (username, hashedPassword.decode('utf-8'),))
-        conn.commit()
+            cur.execute("INSERT INTO users (username, hashed_pass) VALUES (%s, %s) ", (username, hashedPassword.decode('utf-8'),))
+            conn.commit()
 
-        return jsonify({"message": "User has been registered"}), 200
-    
-    except Exception as e:
-    
-        print("Error: ", e)
-        return jsonify({"error": "Internal Server Error"}), 500
+            return jsonify({"message": "User has been registered"}), 200
+        
+        except Exception as e:
+        
+            print("Error: ", e)
+            return jsonify({"error": "Internal Server Error"}), 500
     
 
 @app.route("/api/login", methods=["POST"])
-
 def loginUsers():
-    try:
-        createTables()
-        data = request.json
-        username = data.get("username")
-        password = data.get("password")
+    conn = getDbConn()
+    with conn.cursor() as cur:
+        try:
+            createTables()
+            data = request.json
+            username = data.get("username")
+            password = data.get("password")
 
-        conn = getDbConnection()
-        cur = conn.cursor()
+            # conn = getDbConnection()
+            # cur = conn.cursor()
 
-        cur.execute("SELECT id, username FROM users WHERE username = %s", (username,))
-        userExists = cur.fetchone()
+            cur.execute("SELECT id, username FROM users WHERE username = %s", (username,))
+            userExists = cur.fetchone()
 
-        if not userExists:
-            return jsonify({"error": "User does not exists"}), 409
+            if not userExists:
+                return jsonify({"error": "User does not exists"}), 409
+            
+            user_id = userExists[0]
+            cur.execute("SELECT hashed_pass from users WHERE username = %s", (username,))
+            user_data = cur.fetchone()
+
+            hashed_pass = user_data[0]
+
+            if bcrypt.checkpw(password.encode('utf-8'), hashed_pass.encode('utf-8')):
+
+                accessToken = create_access_token(identity=str(user_id), expires_delta=timedelta(minutes=15))
+                return jsonify({"message": "Login Success", "access_token": accessToken}), 200
+            
+            return jsonify({"error": "Login Failed"}), 401
         
-        user_id = userExists[0]
-        cur.execute("SELECT hashed_pass from users WHERE username = %s", (username,))
-        user_data = cur.fetchone()
-
-        hashed_pass = user_data[0]
-
-        if bcrypt.checkpw(password.encode('utf-8'), hashed_pass.encode('utf-8')):
-
-            accessToken = create_access_token(identity=str(user_id), expires_delta=timedelta(minutes=15))
-            return jsonify({"message": "Login Success", "access_token": accessToken}), 200
-        
-        return jsonify({"error": "Login Failed"}), 401
-    
-    except Exception as e:
-        return jsonify({"error": "Internal Server Error"}), 500
-    
-    finally:
-        cur.close()
-        conn.close()
+        except Exception as e:
+            return jsonify({"error": "Internal Server Error"}), 500        
 
 @app.route("/api/protected", methods=['GET'])
 @jwt_required()
@@ -153,77 +151,68 @@ def setPassword():
     wb = data.get('websiteName')
     wu = data.get('websiteUser')
     wp = data.get('websitePassword')
-    try:
-        conn = getDbConnection()
-        cur = conn.cursor()
 
-        cipher = Fernet(key)
-        encrypted_pass = cipher.encrypt(wp.encode())
+    conn = getDbConn()
+    with conn.cursor() as cur:
+        try:
+            cipher = Fernet(key)
+            encrypted_pass = cipher.encrypt(wp.encode())
+            
+            cur.execute("INSERT INTO passwords (user_id, website, w_user, password) VALUES (%s, %s, %s, %s)", (user_id, wb, wu, encrypted_pass.decode()))
+
+            conn.commit()
+            return jsonify({"message": "Password Inserted"}), 201
         
-        cur.execute("INSERT INTO passwords (user_id, website, w_user, password) VALUES (%s, %s, %s, %s)", (user_id, wb, wu, encrypted_pass.decode()))
-
-        conn.commit()
-        return jsonify({"message": "Password Inserted"}), 201
-    
-    except Exception as e:
-        return jsonify({"error" : "Internal Server Error", "details": e}), 500
-    
-    finally:
-        cur.close()
-        conn.close()
+        except Exception as e:
+            return jsonify({"error" : "Internal Server Error", "details": e}), 500
+        
 
 @app.route("/api/getPasswords", methods=['GET'])
 @jwt_required()
 def getPasswords():
-    try:
-        user_id = get_jwt_identity()
-        conn = getDbConnection()
-        cur = conn.cursor()
-        cipher = Fernet(key)
+    conn = getDbConn()
+    with conn.cursor() as cur:
+        try:
+            user_id = get_jwt_identity()
+            cipher = Fernet(key)
+            cur.execute("SELECT id, website, w_user, password FROM passwords WHERE user_id = %s", (user_id,))
+            data = cur.fetchall()
+            processed = []
+            for item in data:
+                new_item = list(item)
+                new_item[3] = (cipher.decrypt(new_item[3])).decode()
+                processed.append(tuple(new_item))
 
-        cur.execute("SELECT id, website, w_user, password FROM passwords WHERE user_id = %s", (user_id,))
-        data = cur.fetchall()
-        processed = []
-        for item in data:
-            new_item = list(item)
-            new_item[3] = (cipher.decrypt(new_item[3])).decode()
-            processed.append(tuple(new_item))
-
-        return jsonify({"message": "Passwords Retrieved", "data": processed}), 200
-    except Exception as e:
-        print(e)
-        return jsonify({"error" : "Internal Server Error"}), 500
-    finally:
-        cur.close()
-        conn.close()
+            return jsonify({"message": "Passwords Retrieved", "data": processed}), 200
+        except Exception as e:
+            print(e)
+            return jsonify({"error" : "Internal Server Error"}), 500
 
 @app.route("/api/deletePassword", methods=['DELETE'])
 @jwt_required()
 def deletePassword():
-    try:
-        conn = getDbConnection()
-        cur = conn.cursor()
+    conn = getDbConn()
+    with conn.cursor() as cur:
+        try:
+            to_be_deleted_id = request.json.get("id")
+            cur.execute("SELECT * FROM passwords WHERE id = %s", (to_be_deleted_id,))
+            passExists = cur.fetchone()
 
-        to_be_deleted_id = request.json
+            if not passExists:
+                return jsonify({"error": "Password does not exist"}), 401
 
-        cur.execute("SELECT * FROM passwords WHERE id = %s", (to_be_deleted_id,))
-        passExists = cur.fetchone()
+            cur.execute("DELETE FROM passwords WHERE id = %s", (to_be_deleted_id,))
+            conn.commit()
 
-        if not passExists:
-            return jsonify({"error": "Password does not exist"}), 401
+            return jsonify({"message": "Password Deleted"}), 200
 
-        cur.execute("DELETE FROM passwords WHERE id = %s", (to_be_deleted_id,))
-        conn.commit()
-
-        return jsonify({"message": "Password Deleted"}), 200
-
-    except Exception as e:
-        print(e)
-        return jsonify({"error": "Internal Server Error"}), 500
-    
-    finally:
-        cur.close()
-        conn.close()
+        except Exception as e:
+            print(e)
+            return jsonify({"error": "Internal Server Error"}), 500
+        
+        finally:
+            cur.close()
+            conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
