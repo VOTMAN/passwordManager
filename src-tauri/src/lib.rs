6ac07eef0f481ffd400bd3c3707b3aeba8934f67
Arnav <76613041+VOTMAN@users.noreply.github.com>
2025-02-03 20:36:09 +0530

    Remove localhost plugin from tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,8 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-// use tauri::{webview::WebviewWindowBuilder, WebviewUrl, Url};
 
 pub fn run() {
-    // let port: u16 = 5173;
-
     tauri::Builder::default()
-        // .plugin(tauri_plugin_localhost::Builder::new(port).build())
         .setup(move |app| {
-            // let url: Url = format!("http://localhost:{}", port).parse().unwrap();
-            // WebviewWindowBuilder::new(app, "amain".to_string(), WebviewUrl::External(url))
-            //   .title("Localhost")
-            //   .build()?;
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
