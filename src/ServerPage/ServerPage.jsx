8508ba4b5067dd595866d65beeabc7832224c363
Arnav <76613041+VOTMAN@users.noreply.github.com>
2026-05-03 10:56:58 +0530

    Change default url to localhost:5000

--- src/ServerPage/ServerPage.jsx
+++ src/ServerPage/ServerPage.jsx
@@ -37,7 +37,7 @@ const ServerPage = () => {
                         </button>
                         <button
                             className={`${styles.button} ${styles.resetButton}`}
-                            onClick={() => handleUserServer("https://pws.vnarnav.online/")}
+                            onClick={() => handleUserServer("http://127.0.0.1:5000")}
                         >
                             Reset to Default
                         </button>
